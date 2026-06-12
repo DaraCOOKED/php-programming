d9c3a68d51e9145309ece315e2dc8faacc04a4f5
DaraCOOKED <dara.somnag020@gmail.com>
2026-06-12 15:53:07 +0700

    feat/heLLO

---
+++ php-week1/live-coding/index.php
@@ -0,0 +1,175 @@
+<?php
+
+   for ($i = 1; $i <= 1; $i++ ){
+
+      echo "*";
+   }
+   echo "<br>";
+
+   for ($i = 1; $i <= 2; $i++ ){
+
+    echo "*";
+   }
+   echo "<br>";
+   for ($i = 1; $i <= 3; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 4; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 5; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 6; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 7; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 8; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 9; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 10; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   
+    for ($i = 1; $i <= 11; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 12; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 13; $i++ ){
+
+    echo "*";
+   }
+   echo "<br>";
+   for ($i = 1; $i <= 14; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 15; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 16; $i++ ){
+
+    echo "*";
+   }
+   echo "<br>";
+   for ($i = 1; $i <= 17; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 18; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 19; $i++ ){
+
+    echo "*";
+   }
+   echo "<br>";
+   for ($i = 1; $i <= 20; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 21; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 22; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 23; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 24; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 25; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   
+    for ($i = 1; $i <= 26; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 27; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 28; $i++ ){
+
+    echo "*";
+   }
+   echo "<br>";
+   for ($i = 1; $i <= 29; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 30; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 31; $i++ ){
+
+    echo "*";
+   }
+   echo "<br>";
+   for ($i = 1; $i <= 32; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 33; $i++ ){
+
+    echo "*";
+   }
+    echo "<br>";
+   for ($i = 1; $i <= 34; $i++ ){
+
+    echo "*";
+   }
+?>
